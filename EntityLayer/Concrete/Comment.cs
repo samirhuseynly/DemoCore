b4204878a8e827ed8926a6e00135790897aa8911
Samir Huseynli <samirhueynli2803@gmail.com>
2021-10-05 22:15:31 +0400

    added entity classes

---
+++ EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public int CommentUserName { get; set; }
+        public int CommentTitle { get; set; }
+        public int CommentContent { get; set; }
+        public DateTime CommentDate { get; set; }
+        public bool CommentStatus { get; set; }
+    }
+}
