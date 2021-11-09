d3afeeb8380a1a1676927619477c774a424d3520
Samir Huseynli <samirhueynli2803@gmail.com>
2021-11-09 23:11:38 +0400

    Add message and notifications

--- CoreDemo/ViewComponents/Writer/WritetMessageNotification.cs
+++ CoreDemo/ViewComponents/Writer/WritetMessageNotification.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,13 @@ namespace CoreDemo.ViewComponents.Writer
 {
     public class WritetMessageNotification : ViewComponent
     {
+        MessageManager messageManager = new MessageManager(new EfMessageRepository());
         public IViewComponentResult Invoke()
         {
-            return View();
+            string p;
+            p = "samirhueynli2803@gmail.com";
+            var value = messageManager.GetInboxListByWriter(p);
+            return View(value);
         }
     }
 }
